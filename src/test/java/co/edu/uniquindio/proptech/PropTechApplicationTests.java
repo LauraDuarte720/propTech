9654bf9caad9d71febc1992daecedf9716034b02
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2026-03-31 10:44:09 -0500

    chore: Initialize project setup

---
+++ src/test/java/co/edu/uniquindio/proptech/PropTechApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.proptech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
