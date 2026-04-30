de20de2a71419c0aefe9ad7824f58c7a939282e2
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2026-04-30 18:41:54 -0500

    feat: Add service (CRUD) for Property

--- src/main/java/co/edu/uniquindio/com/proptech/repositories/impl/PropertyRepositoryImpl.java
+++ src/main/java/co/edu/uniquindio/com/proptech/repositories/impl/PropertyRepositoryImpl.java
@@ -3,6 +3,8 @@ package co.edu.uniquindio.com.proptech.repositories.impl;
 import co.edu.uniquindio.com.proptech.domain.model.Property;
 import co.edu.uniquindio.com.proptech.domain.model.PropTech;
 import co.edu.uniquindio.com.proptech.repositories.PropertyRepository;
+import co.edu.uniquindio.com.proptech.structures.hashTable.HashTable;
+
 import java.util.Optional;
 
 public class PropertyRepositoryImpl implements PropertyRepository {
@@ -26,5 +28,10 @@ public class PropertyRepositoryImpl implements PropertyRepository {
     @Override
     public boolean deleteById(String code) {
         return propTech.removeProperty(code);
+    }
+
+    @Override
+    public HashTable<String, Property> getProperties() {
+        return propTech.getProperties();
     }
 }
---
+++ src/main/java/co/edu/uniquindio/com/proptech/services/PropertyService.java
@@ -0,0 +1,47 @@
+package co.edu.uniquindio.com.proptech.services;
+
+import co.edu.uniquindio.com.proptech.domain.model.Property;
+import co.edu.uniquindio.com.proptech.repositories.impl.PropertyRepositoryImpl;
+import co.edu.uniquindio.com.proptech.structures.hashTable.HashTable;
+
+public class PropertyService {
+
+    PropertyRepositoryImpl propertyRepository;
+
+    public void registerProperty(Property property) {
+        boolean exists = propertyRepository.findByCode(property.getCode()).isPresent();
+
+        if (exists) {
+            throw new RuntimeException("Ya existe una propiedad con ese código");
+        }
+
+        propertyRepository.save(property);
+    }
+
+    public void updateProperty(Property property) {
+        if (propertyRepository.findByCode(property.getCode()).isEmpty()) {
+            throw new RuntimeException("No existe una propiedad con ese código");
+        }
+
+        propertyRepository.save(property);
+    }
+
+    public void deleteProperty(Property property) {
+        if (propertyRepository.findByCode(property.getCode()).isEmpty()) {
+            throw new RuntimeException("No existe una propiedad con ese código");
+        }
+
+        propertyRepository.deleteById(property.getCode());
+
+    }
+
+    public HashTable<String, Property> getAllProperties() {
+        HashTable<String, Property> properties = propertyRepository.getProperties();
+
+        if (properties == null || properties.isEmpty()) {
+            throw new RuntimeException("No hay propiedades registradas");
+        }
+
+        return properties;
+    }
+}
