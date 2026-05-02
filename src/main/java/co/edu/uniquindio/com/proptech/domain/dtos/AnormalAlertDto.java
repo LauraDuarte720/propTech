8008fcfa5b76e7b61746cdb94b9217809571b7bc
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-05-01 22:45:17 -0500

    feat: Add validations for alert dtos

--- src/main/java/co/edu/uniquindio/com/proptech/domain/dtos/AnormalAlertDto.java
+++ src/main/java/co/edu/uniquindio/com/proptech/domain/dtos/AnormalAlertDto.java
@@ -1,7 +1,11 @@
 package co.edu.uniquindio.com.proptech.domain.dtos;
 
+
+import co.edu.uniquindio.com.proptech.domain.dtos.AgentDtoReturn;
 import co.edu.uniquindio.com.proptech.domain.enums.AlertAnormalType;
 import co.edu.uniquindio.com.proptech.domain.enums.AttentionLevel;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,21 +23,29 @@ public class AnormalAlertDto {
 
     private String id;
 
+    @NotNull(message = "Timestamp is required")
     private LocalDateTime timestamp;
 
     private boolean reviewed;
 
+    @Valid
     private AgentDtoReturn agent;
 
+    @Valid
     private ClientDtoReturn client;
 
+    @Valid
     private OperationDtoCreate operation;
 
+    @Valid
     private PropertyDtoReturn property;
 
+    @Valid
     private VisitDtoReturn visit;
 
+    @NotNull(message = "Alert type is required")
     private AlertAnormalType alertAnormalType;
 
+    @NotNull(message = "Attention level is required")
     private AttentionLevel attentionLevel;
 }
--- src/main/java/co/edu/uniquindio/com/proptech/domain/dtos/BasicAlertDto.java
+++ src/main/java/co/edu/uniquindio/com/proptech/domain/dtos/BasicAlertDto.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.com.proptech.domain.dtos;
-
 import co.edu.uniquindio.com.proptech.domain.enums.AlertType;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,20 +19,27 @@ public class BasicAlertDto {
 
     private String id;
 
+    @NotNull(message = "Timestamp is required")
+    @PastOrPresent(message = "Timestamp cannot be in the future")
     private LocalDateTime timestamp;
 
     private boolean reviewed;
 
+    @Valid
     private AgentDtoReturn agent;
 
+    @Valid
     private ClientDtoReturn client;
 
+    @Valid
     private OperationDtoCreate operation;
 
+    @Valid
     private PropertyDtoReturn property;
 
+    @Valid
     private VisitDtoReturn visit;
 
+    @NotNull(message = "Alert type is required")
     private AlertType alertType;
-
 }
