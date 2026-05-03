edd7be63ab5f655351a61b663da3a650280099fd
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-05-02 20:18:32 -0500

    feat: Add mappers for alerts

--- src/main/java/co/edu/uniquindio/com/proptech/config/mappers/impl/AnormalAlertMapper.java
+++ src/main/java/co/edu/uniquindio/com/proptech/config/mappers/impl/AnormalAlertMapper.java
@@ -1,4 +1,23 @@
 package co.edu.uniquindio.com.proptech.config.mappers.impl;
 
-public class AnormalAlertMapper {
+import co.edu.uniquindio.com.proptech.config.mappers.MapperSimple;
+import co.edu.uniquindio.com.proptech.domain.dtos.AnormalAlertDto;
+import co.edu.uniquindio.com.proptech.domain.model.AnormalAlert;
+
+public class AnormalAlertMapper implements MapperSimple<AnormalAlert, AnormalAlertDto> {
+    @Override
+    public AnormalAlertDto toDto(AnormalAlert entity) {
+        return AnormalAlertDto.builder()
+                .id(entity.getId())
+                .timestamp(entity.getTimestamp())
+                .reviewed(entity.isReviewed())
+                .alertAnormalType(entity.getAlertAnormalType())
+                .attentionLevel(entity.getAttentionLevel())
+                .agent(null)
+                .client(null)
+                .operation(null)
+                .property(null)
+                .visit(null)
+                .build();
+    }
 }
--- src/main/java/co/edu/uniquindio/com/proptech/config/mappers/impl/BasicAlertMapper.java
+++ src/main/java/co/edu/uniquindio/com/proptech/config/mappers/impl/BasicAlertMapper.java
@@ -1,4 +1,23 @@
 package co.edu.uniquindio.com.proptech.config.mappers.impl;
 
-public class BasicAlertMapper {
+import co.edu.uniquindio.com.proptech.config.mappers.MapperSimple;
+import co.edu.uniquindio.com.proptech.domain.dtos.BasicAlertDto;
+import co.edu.uniquindio.com.proptech.domain.model.BasicAlert;
+
+public class BasicAlertMapper implements MapperSimple<BasicAlert, BasicAlertDto> {
+
+    @Override
+    public BasicAlertDto toDto(BasicAlert entity) {
+        return BasicAlertDto.builder()
+                .id(entity.getId())
+                .timestamp(entity.getTimestamp())
+                .reviewed(entity.isReviewed())
+                .alertType(entity.getAlertType())
+                .agent(null)
+                .client(null)
+                .operation(null)
+                .property(null)
+                .visit(null)
+                .build();
+    }
 }
