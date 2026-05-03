1418a6fbe9d7b6df57ec3b6b3b3fe174385a3e14
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-05-02 19:57:19 -0500

    feat: Implement agent mapper and change name of method in its interface

--- src/main/java/co/edu/uniquindio/com/proptech/config/mappers/Mapper.java
+++ src/main/java/co/edu/uniquindio/com/proptech/config/mappers/Mapper.java
@@ -3,5 +3,5 @@ package co.edu.uniquindio.com.proptech.config.mappers;
 public interface Mapper<E, C, U, R> {
     E toEntity(C createDto);
     R toDto(E entity);
-    void updateEntity(U updateDto, E entity);
+    E toUpdate(U updateDto);
 }
--- src/main/java/co/edu/uniquindio/com/proptech/config/mappers/impl/AgentMapper.java
+++ src/main/java/co/edu/uniquindio/com/proptech/config/mappers/impl/AgentMapper.java
@@ -1,4 +1,48 @@
 package co.edu.uniquindio.com.proptech.config.mappers.impl;
 
-public class AgentMapper {
+import co.edu.uniquindio.com.proptech.config.mappers.Mapper;
+import co.edu.uniquindio.com.proptech.domain.dtos.AgentDtoCreate;
+import co.edu.uniquindio.com.proptech.domain.dtos.AgentDtoUpdate;
+import co.edu.uniquindio.com.proptech.domain.dtos.AgentDtoReturn;
+import co.edu.uniquindio.com.proptech.domain.model.Agent;
+
+public class AgentMapper implements Mapper<Agent, AgentDtoCreate, AgentDtoUpdate, AgentDtoReturn> {
+
+    @Override
+    public Agent toEntity(AgentDtoCreate dto) {
+        return Agent.builder()
+                .cedula(dto.getCedula())
+                .name(dto.getName())
+                .username(dto.getUsername())
+                .password(dto.getPassword())
+                .contact(dto.getContact())
+                .closedDeals(dto.getClosedDeals())
+                .assignedZone(null) // map separately if needed (GeographicZoneMapper)
+                .build();
+    }
+
+    @Override
+    public AgentDtoReturn toDto(Agent entity) {
+        return AgentDtoReturn.builder()
+                .cedula(entity.getCedula())
+                .name(entity.getName())
+                .username(entity.getUsername())
+                .contact(entity.getContact())
+                .closedDeals(entity.getClosedDeals())
+                .assignedZone(null) // map separately if needed
+                .build();
+    }
+
+    @Override
+    public Agent toUpdate(AgentDtoUpdate dto) {
+        return Agent.builder()
+                .cedula(dto.getCedula())
+                .name(dto.getName())
+                .username(dto.getUsername())
+                .password(dto.getPassword())
+                .contact(dto.getContact())
+                .closedDeals(dto.getClosedDeals())
+                .assignedZone(null) // map separately if needed (GeographicZoneMapper)
+                .build();
+    }
 }
