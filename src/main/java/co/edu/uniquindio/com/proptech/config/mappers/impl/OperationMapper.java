73c9c96e6a0d3e9c096d1180d5173a3c40fb7868
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-05-02 20:38:21 -0500

    feat: Implement mapper for operation

--- src/main/java/co/edu/uniquindio/com/proptech/config/mappers/impl/OperationMapper.java
+++ src/main/java/co/edu/uniquindio/com/proptech/config/mappers/impl/OperationMapper.java
@@ -1,4 +1,57 @@
 package co.edu.uniquindio.com.proptech.config.mappers.impl;
 
-public class OperationMapper {
+import co.edu.uniquindio.com.proptech.config.mappers.MapperCrud;
+import co.edu.uniquindio.com.proptech.domain.dtos.OperationDtoCreate;
+import co.edu.uniquindio.com.proptech.domain.dtos.OperationDtoUpdate;
+import co.edu.uniquindio.com.proptech.domain.dtos.OperationDtoReturn;
+import co.edu.uniquindio.com.proptech.domain.model.Operation;
+
+public class OperationMapper implements MapperCrud<Operation, OperationDtoCreate, OperationDtoUpdate, OperationDtoReturn> {
+
+    @Override
+    public Operation toEntity(OperationDtoCreate dto) {
+        return Operation.builder()
+                .property(null)
+                .client(null)
+                .agent(null)
+                .dateInitial(dto.getDateInitial())
+                .dateFinal(dto.getDateFinal())
+                .operationType(dto.getOperationType())
+                .value(dto.getValue())
+                .commission(dto.getCommission())
+                .processStatus(dto.getProcessStatus())
+                .build();
+    }
+
+    @Override
+    public OperationDtoReturn toDto(Operation entity) {
+        return OperationDtoReturn.builder()
+                .id(entity.getId())
+                .property(null)
+                .client(null)
+                .agent(null)
+                .dateInitial(entity.getDateInitial())
+                .dateFinal(entity.getDateFinal())
+                .operationType(entity.getOperationType())
+                .value(entity.getValue())
+                .commission(entity.getCommission())
+                .processStatus(entity.getProcessStatus())
+                .build();
+    }
+
+    @Override
+    public Operation toUpdate(OperationDtoUpdate dto) {
+        return Operation.builder()
+                .id(dto.getId())
+                .property(null)
+                .client(null)
+                .agent(null)
+                .dateInitial(dto.getDateInitial())
+                .dateFinal(dto.getDateFinal())
+                .operationType(dto.getOperationType())
+                .value(dto.getValue() != null ? dto.getValue() : 0)
+                .commission(dto.getCommission() != null ? dto.getCommission() : 0)
+                .processStatus(dto.getProcessStatus())
+                .build();
+    }
 }
