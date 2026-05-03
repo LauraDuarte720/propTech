0a6f50af7d60348393fe622edcde381c891290b1
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-05-02 20:26:44 -0500

    feat: Implement ClientMapper

--- src/main/java/co/edu/uniquindio/com/proptech/config/mappers/impl/ClientMapper.java
+++ src/main/java/co/edu/uniquindio/com/proptech/config/mappers/impl/ClientMapper.java
@@ -1,4 +1,60 @@
 package co.edu.uniquindio.com.proptech.config.mappers.impl;
 
-public class ClientMapper {
+import co.edu.uniquindio.com.proptech.config.mappers.MapperCrud;
+import co.edu.uniquindio.com.proptech.domain.dtos.ClientDtoCreate;
+import co.edu.uniquindio.com.proptech.domain.dtos.ClientDtoUpdate;
+import co.edu.uniquindio.com.proptech.domain.dtos.ClientDtoReturn;
+import co.edu.uniquindio.com.proptech.domain.model.Client;
+
+public class ClientMapper implements MapperCrud<Client, ClientDtoCreate, ClientDtoUpdate, ClientDtoReturn> {
+
+    @Override
+    public Client toEntity(ClientDtoCreate dto) {
+        return Client.builder()
+                .cedula(dto.getCedula())
+                .name(dto.getName())
+                .username(dto.getUsername())
+                .password(dto.getPassword())
+                .email(dto.getEmail())
+                .phone(dto.getPhone())
+                .budget(dto.getBudget())
+                .minBedrooms(dto.getMinBedrooms())
+                .clientType(dto.getClientType())
+                .searchStatus(dto.getSearchStatus())
+                .desiredPropertyType(dto.getDesiredPropertyType())
+                .build();
+    }
+
+    @Override
+    public ClientDtoReturn toDto(Client entity) {
+        return ClientDtoReturn.builder()
+                .cedula(entity.getCedula())
+                .name(entity.getName())
+                .username(entity.getUsername())
+                .email(entity.getEmail())
+                .phone(entity.getPhone())
+                .budget(entity.getBudget())
+                .minBedrooms(entity.getMinBedrooms())
+                .clientType(entity.getClientType())
+                .searchStatus(entity.getSearchStatus())
+                .desiredPropertyType(entity.getDesiredPropertyType())
+                .build();
+    }
+
+    @Override
+    public Client toUpdate(ClientDtoUpdate dto) {
+        return Client.builder()
+                .cedula(dto.getCedula()) // obligatorio
+                .name(dto.getName())
+                .username(dto.getUsername())
+                .password(dto.getPassword())
+                .email(dto.getEmail())
+                .phone(dto.getPhone())
+                .budget(dto.getBudget() != null ? dto.getBudget() : 0)
+                .minBedrooms(dto.getMinBedrooms() != null ? dto.getMinBedrooms() : 0)
+                .clientType(dto.getClientType())
+                .searchStatus(dto.getSearchStatus())
+                .desiredPropertyType(dto.getDesiredPropertyType())
+                .build();
+    }
 }
