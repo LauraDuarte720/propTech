774a9d5b7ead12d719864961daff27874c9cc683
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-05-02 20:54:50 -0500

    feat: Implement mapper for visit

--- src/main/java/co/edu/uniquindio/com/proptech/config/mappers/impl/VisitMapper.java
+++ src/main/java/co/edu/uniquindio/com/proptech/config/mappers/impl/VisitMapper.java
@@ -1,4 +1,37 @@
 package co.edu.uniquindio.com.proptech.config.mappers.impl;
 
-public class VisitMapper {
+import co.edu.uniquindio.com.proptech.config.mappers.MapperCrud;
+import co.edu.uniquindio.com.proptech.domain.dtos.*;
+import co.edu.uniquindio.com.proptech.domain.model.Visit;
+
+public class VisitMapper implements MapperCrud<Visit, VisitDtoCreate, VisitDtoUpdate, VisitDtoReturn> {
+
+    @Override
+    public Visit toEntity(VisitDtoCreate dto) {
+        return Visit.builder()
+                .date(dto.getDate())
+                .status(dto.getStatus())
+                .postVisitNotes(dto.getPostVisitNotes())
+                .build();
+    }
+
+    @Override
+    public VisitDtoReturn toDto(Visit entity) {
+        return VisitDtoReturn.builder()
+                .id(entity.getId())
+                .date(entity.getDate())
+                .status(entity.getStatus())
+                .postVisitNotes(entity.getPostVisitNotes())
+                .build();
+    }
+
+    @Override
+    public Visit toUpdate(VisitDtoUpdate dto) {
+        return Visit.builder()
+                .id(dto.getId())
+                .date(dto.getDate())
+                .status(dto.getStatus())
+                .postVisitNotes(dto.getPostVisitNotes())
+                .build();
+    }
 }
