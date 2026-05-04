ed177e4705d194efc9acbd6ded7b70185c671985
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-05-03 22:46:55 -0500

    feat: Add application class

---
+++ src/main/java/co/edu/uniquindio/com/proptech/ProptechApplication.java
@@ -0,0 +1,26 @@
+package co.edu.uniquindio.com.proptech;
+
+
+import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+@Log
+public class ProptechApplication {
+
+
+    public ProptechApplication() {
+
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProptechApplication.class, args);
+
+        Runnable a;
+    }
+
+}
