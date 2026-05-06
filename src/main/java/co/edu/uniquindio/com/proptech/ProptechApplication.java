30033ed207e683454151d004e201ffc269830ff5
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2026-05-06 18:55:16 -0500

    feat: Add Utility for schedule the Alert

--- src/main/java/co/edu/uniquindio/com/proptech/ProptechApplication.java
+++ src/main/java/co/edu/uniquindio/com/proptech/ProptechApplication.java
@@ -4,9 +4,10 @@ package co.edu.uniquindio.com.proptech;
 import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-
+@EnableScheduling
 @SpringBootApplication
 @Log
 public class ProptechApplication {
