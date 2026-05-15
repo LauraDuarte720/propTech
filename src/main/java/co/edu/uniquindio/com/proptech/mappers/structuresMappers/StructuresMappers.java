974558e2e001b9d7c14994b77cd96c88bc571579
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-05-15 08:06:46 -0500

    feat: Add module for mapping structures into java Lists mapped to dtos

---
+++ src/main/java/co/edu/uniquindio/com/proptech/mappers/structuresMappers/StructuresMappers.java
@@ -0,0 +1,74 @@
+package co.edu.uniquindio.com.proptech.mappers.structuresMappers;
+
+import co.edu.uniquindio.com.proptech.mappers.MapperOnlyDto;
+import co.edu.uniquindio.com.proptech.structures.AVLTree.AVLTree;
+import co.edu.uniquindio.com.proptech.structures.arrayList.ArrayList;
+import co.edu.uniquindio.com.proptech.structures.hashTable.HashTable;
+import co.edu.uniquindio.com.proptech.structures.linkedList.LinkedList;
+import co.edu.uniquindio.com.proptech.structures.priorityQueue.PriorityQueue;
+import co.edu.uniquindio.com.proptech.structures.queue.Queue;
+import co.edu.uniquindio.com.proptech.structures.stack.Stack;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StructuresMappers {
+
+    public <E, D> List<D> fromArrayList(ArrayList<E> source, MapperOnlyDto<E, D> mapper) {
+        List<D> result = new java.util.ArrayList<>();
+        for (E element : source) {
+            result.add(mapper.toDto(element));
+        }
+        return result;
+    }
+
+    public <E, D> List<D> fromLinkedList(LinkedList<E> source, MapperOnlyDto<E, D> mapper) {
+        List<D> result = new java.util.ArrayList<>();
+        for (E element : source) {
+            result.add(mapper.toDto(element));
+        }
+        return result;
+    }
+
+    public <E extends Comparable<E>, D> List<D> fromAVLTree(AVLTree<E> source, MapperOnlyDto<E, D> mapper) {
+        List<D> result = new java.util.ArrayList<>();
+        for (E element : source.inOrder()) {
+            result.add(mapper.toDto(element));
+        }
+        return result;
+    }
+
+    public <E, D> List<D> fromStack(Stack<E> source, MapperOnlyDto<E, D> mapper) {
+        List<D> result = new java.util.ArrayList<>();
+        for (E element : source.toJavaList()) {
+            result.add(mapper.toDto(element));
+        }
+        return result;
+    }
+
+    public <E, D> List<D> fromQueue(Queue<E> source, MapperOnlyDto<E, D> mapper) {
+        List<D> result = new java.util.ArrayList<>();
+        for (E element : source.toJavaList()) {
+            result.add(mapper.toDto(element));
+        }
+        return result;
+    }
+
+    public <E, D> List<D> fromPriorityQueue(PriorityQueue<E> source, MapperOnlyDto<E, D> mapper) {
+        List<D> result = new java.util.ArrayList<>();
+        for (E element : source.toJavaList()) {
+            result.add(mapper.toDto(element));
+        }
+        return result;
+    }
+
+    public <K, V, D> List<D> fromHashTableValues(HashTable<K, V> source, MapperOnlyDto<V, D> mapper) {
+        List<D> result = new java.util.ArrayList<>();
+        for (V element : source.valuesToJavaList()) {
+            result.add(mapper.toDto(element));
+        }
+        return result;
+    }
+}
