92698120da9ee0efaaab31678d5b7a76496cbc3f
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-05-21 12:40:32 -0500

    feat: Add stack with snapshots to undo property changes

--- src/main/java/co/edu/uniquindio/com/proptech/domain/model/Property.java
+++ src/main/java/co/edu/uniquindio/com/proptech/domain/model/Property.java
@@ -5,6 +5,7 @@ import co.edu.uniquindio.com.proptech.domain.enums.PropertyStatus;
 import co.edu.uniquindio.com.proptech.domain.enums.PropertyType;
 import co.edu.uniquindio.com.proptech.domain.enums.Purpose;
 import co.edu.uniquindio.com.proptech.structures.linkedList.LinkedList;
+import co.edu.uniquindio.com.proptech.structures.stack.Stack;
 import lombok.*;
 
 
@@ -28,6 +29,9 @@ public class Property implements Comparable<Property> {
     private Agent agent;
 
     @Builder.Default
+    private Stack<PropertySnapshot> history = new Stack<>();
+
+    @Builder.Default
     private LinkedList<PriceHistory> priceHistory = new LinkedList<>();
 
     public boolean isAvailable() {
@@ -37,5 +41,46 @@ public class Property implements Comparable<Property> {
     @Override
     public int compareTo(Property o) {
         return Double.compare(price, o.getPrice());
+    }
+
+    public PropertySnapshot createSnapshot() {
+        return PropertySnapshot.builder()
+                .address(address)
+                .neighborhood(neighborhood)
+                .purpose(purpose)
+                .price(price)
+                .area(area)
+                .numBedrooms(numBedrooms)
+                .numBathrooms(numBathrooms)
+                .status(status)
+                .agent(agent)
+                .build();
+    }
+
+    public void restoreSnapshot(PropertySnapshot snapshot) {
+        this.address = snapshot.getAddress();
+        this.neighborhood = snapshot.getNeighborhood();
+        this.purpose = snapshot.getPurpose();
+        this.price = snapshot.getPrice();
+        this.area = snapshot.getArea();
+        this.numBedrooms = snapshot.getNumBedrooms();
+        this.numBathrooms = snapshot.getNumBathrooms();
+        this.status = snapshot.getStatus();
+        this.agent = snapshot.getAgent();
+    }
+
+    public void saveSnapshot() {
+        history.push(createSnapshot());
+    }
+
+    public PropertySnapshot getLastSnapshot() {
+        if (history.isEmpty()) {
+            return null;
+        }
+        return history.pop();
+    }
+
+    public boolean hasSnapshots() {
+        return !history.isEmpty();
     }
 }
---
+++ src/main/java/co/edu/uniquindio/com/proptech/domain/model/PropertySnapshot.java
@@ -0,0 +1,24 @@
+package co.edu.uniquindio.com.proptech.domain.model;
+
+import co.edu.uniquindio.com.proptech.domain.enums.PropertyStatus;
+import co.edu.uniquindio.com.proptech.domain.enums.Purpose;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PropertySnapshot {
+
+    private String address;
+    private Neighborhood neighborhood;
+    private Purpose purpose;
+    private Double price;
+    private Double area;
+    private Integer numBedrooms;
+    private Integer numBathrooms;
+    private PropertyStatus status;
+    private Agent agent;
+
+}
