a03f6bbf042fd384b07b4bc0207dc029f82fcb1d
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-05-21 13:43:57 -0500

    feat: Add dto and mapper for admin action logs

---
+++ src/main/java/co/edu/uniquindio/com/proptech/domain/dtos/AdminActionLogDtoReturn.java
@@ -0,0 +1,27 @@
+package co.edu.uniquindio.com.proptech.domain.dtos;
+
+import co.edu.uniquindio.com.proptech.domain.enums.AdminActionType;
+import co.edu.uniquindio.com.proptech.domain.enums.AdminEntityType;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AdminActionLogDtoReturn {
+
+    private String id;
+
+    private AdminEntityType entity;
+
+    private AdminActionType action;
+
+    private String description;
+
+    private LocalDateTime timestamp;
+
+    private String performedBy;
+}
---
+++ src/main/java/co/edu/uniquindio/com/proptech/mappers/impl/AdminActionLogMapper.java
@@ -0,0 +1,23 @@
+package co.edu.uniquindio.com.proptech.mappers.impl;
+
+
+import co.edu.uniquindio.com.proptech.domain.dtos.AdminActionLogDtoReturn;
+import co.edu.uniquindio.com.proptech.domain.model.AdminActionLog;
+import co.edu.uniquindio.com.proptech.mappers.MapperOnlyDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminActionLogMapper implements MapperOnlyDto<AdminActionLog, AdminActionLogDtoReturn> {
+
+    @Override
+    public AdminActionLogDtoReturn toDto(AdminActionLog entity) {
+        return AdminActionLogDtoReturn.builder()
+                .id(entity.getId())
+                .entity(entity.getEntity())
+                .action(entity.getAction())
+                .description(entity.getDescription())
+                .timestamp(entity.getTimestamp())
+                .performedBy(entity.getPerformedBy())
+                .build();
+    }
+}
