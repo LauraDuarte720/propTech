f4cbb53ad1205fcdb69f601d0d53c64b4b72c07c
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-05-21 17:54:22 -0500

    feat: Add queue for holding admin action history

--- src/main/java/co/edu/uniquindio/com/proptech/repositories/AdminActionLogRepository.java
+++ src/main/java/co/edu/uniquindio/com/proptech/repositories/AdminActionLogRepository.java
@@ -1,11 +1,12 @@
 package co.edu.uniquindio.com.proptech.repositories;
 
 import co.edu.uniquindio.com.proptech.domain.model.AdminActionLog;
+import co.edu.uniquindio.com.proptech.structures.queue.Queue;
 import co.edu.uniquindio.com.proptech.structures.stack.Stack;
 
 public interface AdminActionLogRepository {
     AdminActionLog save(AdminActionLog log);
-    Stack<AdminActionLog> getHistory();
+    Queue<AdminActionLog> getHistory();
     boolean isEmpty();
     AdminActionLog pop();
 }
