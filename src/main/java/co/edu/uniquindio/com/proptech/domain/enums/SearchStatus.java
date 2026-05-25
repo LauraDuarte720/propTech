dfdc751454e961b8b3fe114625409ddf2e51c23d
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2026-05-25 18:14:42 -0500

    fix: All the enums correspond to the back

--- src/main/java/co/edu/uniquindio/com/proptech/domain/enums/SearchStatus.java
+++ src/main/java/co/edu/uniquindio/com/proptech/domain/enums/SearchStatus.java
@@ -1,5 +1,8 @@
 package co.edu.uniquindio.com.proptech.domain.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * Enum representing the search status of a client.
  */
@@ -19,5 +22,20 @@ public enum SearchStatus {
     @Override
     public String toString() {
         return displayName;
+    }
+
+    @JsonValue
+    public String getKey() {
+        return this.name();
+    }
+
+    @JsonCreator
+    public static SearchStatus fromString(String value) {
+        for (SearchStatus t : values()) {
+            if (t.name().equalsIgnoreCase(value) || t.displayName.equalsIgnoreCase(value)) {
+                return t;
+            }
+        }
+        throw new IllegalArgumentException("Unknown SearchStatus: " + value);
     }
 }
