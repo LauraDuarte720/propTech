7ab06110176276b58dbd97bed7190dc22d418a29
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-05-25 00:50:34 -0500

    feat: Add exception to avoid the updating on a invalid operation

---
+++ src/main/java/co/edu/uniquindio/com/proptech/exceptions/specificExceptions/OperationNotUpdatable.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.com.proptech.exceptions.specificExceptions;
+
+public class OperationNotUpdatable extends RuntimeException {
+    public OperationNotUpdatable(String message) {
+        super(message);
+    }
+}
